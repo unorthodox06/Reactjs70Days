9058df1d92f3ae3f424d41192bad09421ea50bcc
Aniket Kalaskar <unorthodox06@gmail.com>
2025-09-03 15:18:53 +0530

    first day challenge

---
+++ day1/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1 style={{
+        color: 'teal',
+        fontSize: 50
+      }}>Hello Aniket</h1>
+      <p style={{
+        backgroundColor: 'skyblue',
+        fontSize: '60px'
+      }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate asperiores, explicabo itaque, iusto atque ducimus quo voluptatem omnis maxime sint numquam praesentium modi quos corporis suscipit veritatis quidem. Eos, officia.</p>
+    </div>
+  )
+}
+
+export default App
