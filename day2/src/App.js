b6c30d1e5ea8e20d19d2012ec2898259b0936be7
Aniket Kalaskar <unorthodox06@gmail.com>
2025-09-04 14:09:13 +0530

    learn about components

--- day2/src/App.js
+++ day2/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul style={{
+          backgroundColor: 'red',
+          margin: 0,
+          padding: 0,
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'space-around',
+          listStyle: 'none'
+        }}>
+          <li>
+            <a href="#" style={{
+              fontSize: 34,
+              textDecoration: 'none',
+              color: 'white'
+            }}>Home</a></li>
+          <li>
+            <a href="#" style={{
+              fontSize: 34,
+              textDecoration: 'none',
+              color: 'white'
+            }}>Contact us</a></li>
+          <li>
+            <a href="#" style={{
+              fontSize: 34,
+              textDecoration: 'none',
+              color: 'white'
+            }}>About</a></li>
+          <li>
+            <a href="#" style={{
+              fontSize: 34,
+              textDecoration: 'none',
+              color: 'white'
+            }}>Login</a></li>
+
+        </ul>
+
+      </nav>
+
+      <section style={{
+        width: '75%',
+        backgroundColor: '#f4f4f4',
+        padding: 30,
+        margin: 'auto 50px',
+        textAlign: 'center'
+      }}>
+        <h1>This is section</h1>
+      </section>
+
+      <footer style={{
+        backgroundColor: '#b7b7b7b7',
+        textAlign: 'center',
+        padding: 40,
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'center',
+        width: '80%',
+        margin: '50px auto'
+      }}>
+        <h1>This is footer</h1>
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
